e5dd8f331235cad36a3046b355d9a416dae2dad1
mainamurage <adrianmurage21@gmail.com>
2020-02-05 22:57:48 +0300

    Add support for JWTs

--- resources.py
+++ resources.py
@@ -1,3 +1,10 @@
+from flask_jwt_extended import (
+    create_access_token,
+    create_refresh_token,
+    jwt_required,
+    jwt_refresh_token_required,
+    get_jwt_identity
+)
 from flask_restful import Resource, reqparse
 
 from PBP_BE.models import Users
@@ -13,7 +20,8 @@ class RegularUserRegistration(Resource):
         data = regular_user_parser.parse_args()
 
         # if user exists
-        if regular_user.find_user_by_username(data["username"]):
+        current_user = regular_user.find_user_by_username(data["username"])
+        if current_user:
             return {'message': 'User {} already exists'.format(data['username'])}
 
         new_user = {
@@ -22,9 +30,13 @@ class RegularUserRegistration(Resource):
         }
         try:
             regular_user.register_user(new_user)
+            access_token = create_access_token(identity=data['username'])
+            refresh_token = create_refresh_token(identity=data['username'])
             return {
-                'message': 'User {} was created'.format(data["username"])
-            }
+                       'message': 'User {} was successfully created'.format(data["username"]),
+                       'access_token': access_token,
+                       'refresh_token': refresh_token
+                   }, 200
         except:
             return {'message': 'Something went wrong'}, 500
 
@@ -34,11 +46,19 @@ class RegularUserLogin(Resource):
         data = regular_user_parser.parse_args()
         current_user = regular_user.find_user_by_username(data["username"])
         print(current_user)
+
+        # if user does not exist
         if not current_user:
             return {'message': 'User {} doesn\'t exist'.format(data['username'])}
 
         if regular_user.verify_hash(data['password'], current_user['password']):
-            return {'message': 'Logged in as {}'.format(current_user['username'])}
+            access_token = create_access_token(identity=data['username'])
+            refresh_token = create_refresh_token(identity=data['username'])
+            return {
+                       'message': 'Logged in as {}'.format(current_user['username']),
+                       'access_token': access_token,
+                       'refresh_token': refresh_token
+                   }, 200
         else:
             return {'message': 'Wrong credentials'}, 401
 
@@ -54,10 +74,14 @@ class RegularUserLogoutRefresh(Resource):
 
 
 class RegularUserTokenRefresh(Resource):
+    @jwt_refresh_token_required
     def post(self):
-        return {'message': 'regular user token refresh'}
+        current_user = get_jwt_identity()
+        access_token = create_access_token(identity=current_user)
+        return {'access_token': access_token}, 200
 
 
 class SecretResource(Resource):
+    @jwt_required
     def get(self):
         return {'value': True}
--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_jwt_extended import JWTManager
 from flask_restful import Api
 
 app = Flask(__name__)
 api = Api(app)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+jwt = JWTManager(app)
+
+app.config['JWT_SECRET_KEY'] = 'JrR&A7z3#jdxpASvAE18J$%m0tb9TK@P'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
 
 from PBP_BE import resources
 
