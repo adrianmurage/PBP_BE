c19c5c9fafe24eeca612a30487bd4ded283c6f22
mainamurage <adrianmurage21@gmail.com>
2020-02-07 15:46:21 +0300

    reformat file

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_restful import Api
-from flask import jsonify
 
 import resources
 
 app = Flask(__name__)
 api = Api(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+jwt = JWTManager(app)
+
+app.config['JWT_SECRET_KEY'] = 'JrR&A7z3#jdxpASvAE18J$%m0tb9TK@P'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
 
 
 @app.route('/')
 def index():
     return jsonify({'message': 'Hello, World!'})
 
-
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
-jwt = JWTManager(app)
-
-app.config['JWT_SECRET_KEY'] = 'JrR&A7z3#jdxpASvAE18J$%m0tb9TK@P'
-app.config['JWT_ACCESS_TOKEN_EXPIRES'] = False
 
 api.add_resource(resources.RegularUserRegistration, '/api/register')
 api.add_resource(resources.RegularUserLogin, '/api/login')
